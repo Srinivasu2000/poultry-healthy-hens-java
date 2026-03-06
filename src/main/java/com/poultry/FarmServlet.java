85025e12dcf328024155d3b5aab5c4e8f4e0f740
Uday-63 <unofficialuday2002@gmail.com>
2026-03-06 17:03:54 +0530

    Update FarmServlet.java

--- src/main/java/com/poultry/FarmServlet.java
+++ src/main/java/com/poultry/FarmServlet.java
@@ -1,9 +1,8 @@
 package com.poultry;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class FarmServlet extends HttpServlet {
 
@@ -11,11 +10,6 @@ public class FarmServlet extends HttpServlet {
             throws ServletException, IOException {
 
         response.setContentType("text/html");
-
-        PrintWriter out = response.getWriter();
-
-        out.println("<h2>Welcome to Healthy Hens Poultry Farm</h2>");
-        out.println("<p>Farm Location: Andhra Pradesh</p>");
-        out.println("<p>Contact: 9876543210</p>");
+        response.getWriter().println("<h1>Welcome to Healthy Hens Poultry Farm</h1>");
     }
 }
